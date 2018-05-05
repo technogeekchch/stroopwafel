6e6ffb61e78120433013482b8a9578f5d89b6282
GewoonYorick <github@gewoonyorick.nl>
2018-05-05 10:08:51 +0200

    changed def's

--- plugins/basics.py
+++ plugins/basics.py
@@ -2,9 +2,9 @@ from cloudbot import hook
 
 
 @hook.command("info", "information", "botinfo")
-def mycmd(notice):
+def infocmd(notice):
     notice("Youth Bot - This bot it managed by Youth Team")
 
 @hook.command("help", "bothelp")
-def mycmd2(notice):
+def helpcmd(notice):
     notice("Commands you may use (wil be added later)")
