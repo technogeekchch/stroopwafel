9d41c512f72bdd53aea2fbd72d3ad3d76bb7ab2f
GewoonYorick <github@gewoonyorick.nl>
2018-05-05 10:06:28 +0200

    Removed code, changed to notice user instead of in channel msg

--- plugins/basics.py
+++ plugins/basics.py
@@ -1,20 +1,10 @@
 from cloudbot import hook
 
-helpmsg = ["Commands you may use (wil be added later)"]
 
-infomsg = ["Youth Bot - This bot it managed by Youth Team"]
+@hook.command("info", "information", "botinfo")
+def mycmd(notice):
+    notice("Youth Bot - This bot it managed by Youth Team")
 
-
-
-
-
-
-
-
-@hook.command(info, information, botinfo)
-def mycmd(message):
-    message(infomsg)
-
-@hook.command(help, bothelp)
-def mycmd2(message):
-    message(helpmsg)
+@hook.command("help", "bothelp")
+def mycmd2(notice):
+    notice("Commands you may use (wil be added later)")
