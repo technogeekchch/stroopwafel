9b88f9486dbf442dd611ac907e7f4fe85eafb81e
Yorick <yorick_bosman@live.nl>
2018-09-22 22:28:03 +0200

    Removing some default plugins

--- plugins/gamemover.py
+++
@@ -1,9 +0,0 @@
-from cloudbot import hook
-
-
-@hook.command(permissions=["playwerewolf"]"werewolf", "mafia")
-def playgamecmd(nick, notice,conn):
-    notice("You will be connected to the miningame werewolf!")
-	conn.send("SAJOIN '" + nick + "' #youth-werewolf")
-	
-
--- plugins/zncregister.py
+++
@@ -1,6 +0,0 @@
-from cloudbot import hook
-
-
-@hook.command(permissions=["zncadmin"])
-def add(text, message):
-    message(text)
